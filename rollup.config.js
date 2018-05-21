67a1e240ac1313c465088d367d34d2d0c3780571
Alexandre Girard <git@alexgirard.com>
2018-05-21 14:03:49 +0200

    build: add new rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
-import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
 
 export default {
-  input: 'src/dd.js',
-  output: {
-    file: 'dist/deepdetect-browser.min.js',
-    format: 'iife',
-    name: 'deepdetect',
-    footer: '/* DeepDetect JS Client - https://github.com/jolibrain/deepdetect-js */',
-  },
+  entry: 'src/index.js',
+  format: 'cjs',
   plugins: [
-    resolve({ jsnext: true, preferBuiltins: true, browser: true }),
-    uglify(),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      presets: [
-        [
-          'env',
-          { modules: false },
-        ],
-      ],
-      plugins: [
-        'external-helpers',
-      ],
-    }),
+    replace({ 'process.browser': !!process.env.BROWSER }),
   ],
 };
